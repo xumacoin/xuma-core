613afb221a978be1a917544f3d6bf01749db270b
Fly <3713548+flyinghuman@users.noreply.github.com>
2018-10-18 08:33:16 +0200

    Update bitcoingui.cpp

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -486,14 +486,12 @@ void BitcoinGUI::createActions(const NetworkStyle* networkStyle)
 
 void BitcoinGUI::createMenuBar()
 {
-    appMenuBar = menuBar();
 #ifdef Q_OS_MAC
     // Create a decoupled menu bar on Mac which stays even if the window is closed
-    ////appMenuBar = new QMenuBar();
-    appMenuBar->setNativeMenuBar(false);
+    appMenuBar = new QMenuBar();
 #else
     // Get the main window's menu bar on other platforms
-    ////appMenuBar = menuBar();
+    appMenuBar = menuBar();
 #endif
 
     // Configure the menus
