80e870d2cb75638175032e266e7e89ae67ecc56b
Fly <github@c-fi.de>
2018-10-29 10:59:53 +0100

    revert pivx updates because build failures

--- src/util.h
+++ src/util.h
@@ -29,7 +29,6 @@
 
 #include <boost/filesystem/path.hpp>
 #include <boost/thread/exceptions.hpp>
-#include <boost/thread/condition_variable.hpp> // for boost::thread_interrupted
 
 //XUMA only features
 
