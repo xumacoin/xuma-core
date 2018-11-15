faa24a0a3fb75d0cf95bea635c45f8e11226cc09
Fly <3713548+flyinghuman@users.noreply.github.com>
2018-11-15 13:26:57 +0100

    Update test_06_script.sh

--- .travis/test_06_script.sh
+++ .travis/test_06_script.sh
@@ -81,7 +81,7 @@ if [ "$DEPLOY_TEST_BUILDS" = "true" ] && [ "$BUILD_ONLY_DEPENDS" = "false" ]; th
   BEGIN_FOLD deploytests
   DOCKER_EXEC export VERSION="$REASON-$TRAVIS_BRANCH"
   if [ "$REASON" = "MacOS" ]; then DOCKER_EXEC pwd && find . -name '*.dmg' && echo $OUTDIR && mkdir -p $OUTDIR && cp *.dmg $OUTDIR/XUMA-$REASON-$VERSION.dmg && ls; fi
-  if [ "$REASON" != "MacOS" ]; then DOCKER_EXEC pwd && cd $OUTDIR && strip bin/* && mv bin XUMA-$VERSION && ls XUMA-$VERSION/ && zip -r XUMA-$VERSION.zip XUMA-$VERSION && ls; fi
+  if [ "$REASON" != "MacOS" ]; then DOCKER_EXEC pwd && find $OUTDIR && cd $OUTDIR && strip bin/* && mv bin XUMA-$VERSION && ls XUMA-$VERSION/ && zip -r XUMA-$VERSION.zip XUMA-$VERSION && ls; fi
   DOCKER_EXEC git init
   DOCKER_EXEC git config --global user.email "3713548+flyinghuman@users.noreply.github.com"
   DOCKER_EXEC git config --global user.name "Travis-User"
